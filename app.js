d600306e2b403eff14c3c0bab3777cc5a11dfd97
Mohammad Kaif Ali <kaifali744@gmail.com>
2023-07-17 12:41:10 +0530

    Created Meal app
    Created Meal app using html, css and js along with meal api for coding ninjas frontend skill tag

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
+// Get references to the necessary elements
 const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
 const mealList = document.getElementById("meal-list");
 
+// Add event listener to the search button
 searchBtn.addEventListener("click", () => {
-  const searchTerm = searchInput.value.trim();
-  if (searchTerm !== "") {
-    searchMeals(searchTerm);
-  }
+    const searchTerm = searchInput.value.trim();
+    if (searchTerm !== "") {
+        searchMeals(searchTerm);
+    }
 });
 
+// Function to search for meals using the API
 function searchMeals(searchTerm) {
-  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.meals) {
-        displayMeals(data.meals);
-      } else {
-        mealList.innerHTML = "<p>No meals found. Please try again.</p>";
-      }
-    })
-    .catch((error) => {
-      mealList.innerHTML = "<p>An error occurred. Please try again later.</p>";
-      console.error(error);
-    });
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.meals) {
+                displayMeals(data.meals);
+            } else {
+                mealList.innerHTML = "<p>No meals found. Please try again.</p>";
+            }
+        })
+        .catch((error) => {
+            mealList.innerHTML = "<p>An error occurred. Please try again later.</p>";
+            console.error(error);
+        });
 }
 
+// Function to display the meals on the page
 function displayMeals(meals) {
-  mealList.innerHTML = "";
-  meals.forEach((meal) => {
-    const mealCard = document.createElement("div");
-    mealCard.classList.add("col-md-4", "mb-4");
-    mealCard.innerHTML = `
+    mealList.innerHTML = "";
+    meals.forEach((meal) => {
+        const mealCard = document.createElement("div");
+        mealCard.classList.add("col-md-4", "mb-4");
+        mealCard.innerHTML = `
       <div class="card">
         <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}">
         <div class="card-body">
@@ -38,6 +42,6 @@ function displayMeals(meals) {
         </div>
       </div>
     `;
-    mealList.appendChild(mealCard);
-  });
+        mealList.appendChild(mealCard);
+    });
 }
